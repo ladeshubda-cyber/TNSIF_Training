2542e2ace470f14ad736c5fb55045d5ba455668c
Shubda <ladeshubda@gmail.com>
2026-07-16 10:53:50 +0530

    First commit

---
+++ TNSIF_Java/src/org/tnsif/acc/c2tc/java/VariableDemo.java
@@ -0,0 +1,18 @@
+package org.tnsif.acc.c2tc.java;
+
+public class VariableDemo {
+	int price=10;//instance variable
+	void print(){
+		String msg="Hello";//local variable
+		System.out.println(msg);
+	}
+	static String message="Hello Students";//static
+	
+	public static void main(String[] args) {
+		VariableDemo obj=new VariableDemo();
+		System.out.println(obj.price);
+		obj.print();
+		System.out.println(message);
+	}
+
+}
