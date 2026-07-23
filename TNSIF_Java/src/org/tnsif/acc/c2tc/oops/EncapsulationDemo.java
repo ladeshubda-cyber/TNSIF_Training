79442ce1b5d508d43fcee3b5957b84a375e65e3e
Shubda <ladeshubda@gmail.com>
2026-07-23 11:27:37 +0530

    first commit

---
+++ TNSIF_Java/src/org/tnsif/acc/c2tc/oops/EncapsulationDemo.java
@@ -0,0 +1,42 @@
+package org.tnsif.acc.c2tc.oops;
+class Human{
+	//variables
+	private String name;
+	private int age;
+	
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getAge() {
+		return age;
+	}
+	
+}
+
+public class EncapsulationDemo {
+
+	public static void main(String[] args) {
+		Human obj=new Human();
+		obj.setAge(21);
+		obj.setName("Shubda");
+		System.out.println(obj.getAge());
+		System.out.println(obj.getName());
+		
+		
+		obj.setAge(22);
+		obj.setName("Amulya");
+		System.out.println(obj.getAge());
+		System.out.println(obj.getName());
+		
+		
+
+	}
+
+}
